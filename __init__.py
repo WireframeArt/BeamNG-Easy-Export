03f650c38699236c6ead81f7da98b827faca5353
WireframeArt <96269265+WireframeArt@users.noreply.github.com>
2023-08-25 21:18:39 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 #    <Beamng Easy Export, Blender addon for exporting objects to BeamNG.>
-#    Copyright (C) <2022> <Damian Paterson>
+#    Copyright (C) <2023> <Damian Paterson>
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
